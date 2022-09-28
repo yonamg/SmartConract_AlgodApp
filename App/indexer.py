9c6cc25a0cb37d79ae41b1c80455a2df28ee6019
yonamg <softeng2006@gmail.com>
2022-09-28 22:25:59 +0300

    initialize indexer

--- App/indexer.py
+++ App/indexer.py
@@ -1,3 +1,16 @@
 #import required packages
 from algosdk.constants import microalgos_to_algos_ratio
 from algosdk.v2client import indexer
+
+def myindexer():
+    """Initialize and return an indexer"""
+
+    algod_address = "https://testnet-algorand.api.purestake.io/idx2"
+
+    algod_token = "o3UIhhhoqx6IRyNxrfBZg4EBsRhyOTQC58EM1XRw "
+
+    headers = {
+        "X-API-Key": algod_token,
+    }
+
+    return indexer.IndexerClient("", algod_address, headers)
