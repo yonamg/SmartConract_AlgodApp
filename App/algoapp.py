91e3b7e64a5bfe5e3247de76cd2082fd1a0b21dc
yonamg <softeng2006@gmail.com>
2022-09-28 20:00:38 +0300

    initialize algorad app

---
+++ App/algoapp.py
@@ -0,0 +1,6 @@
+# import algoradd sdk libraries
+from algosdk import account, mnemonic
+from algosdk.constants import microalgos_to_algos_ratio
+from algosdk.future.transaction import PaymentTxn, AssetConfigTxn
+from algosdk.v2client import algod
+
